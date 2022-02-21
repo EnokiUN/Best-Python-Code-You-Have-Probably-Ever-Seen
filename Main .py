6888b15a7ef153eac44b3c2139261badd0875b34
Enoki <77694265+EnokiUN@users.noreply.github.com>
2022-02-21 22:58:02 +0300

    Improved perfection (They said it couldn't be done)

---
+++ Main .py
@@ -0,0 +1,23 @@
+import random
+
+import math
+
+
+
+
+import io, sys
+def print(num1:str, num:bool=69)-> None:
+
+ def shhsh(nam, nem):
+          if not True is not (not False):
+                     return num1+nem
+ return shhsh(1,2)
+ 
+sys.stdout.write(
+str(print(1,
+
+
+
+                4)))  
+
+print ("Hello, World!);
