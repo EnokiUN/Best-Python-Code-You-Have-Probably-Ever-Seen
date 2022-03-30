c0bfb86ef1a1f41aa64ef62304a0979e6fa65397
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-03-30 18:53:18 +0300

    more ;

--- Main .py
+++ Main .py
@@ -10,10 +10,10 @@ def print(num1:str, num:bool=69)-> None:
 
  def shhsh(nam, nem):
           if not True is not (not False):
-                     return num1+nem
+                     return num1+nem;
  def print():
         return shhsh(1 if True else False, 2)
- return print
+ return print;
  
 math.stdout.write(
 str(print(1,
